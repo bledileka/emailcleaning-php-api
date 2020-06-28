9bc9b02e2519737cb826c4783e07026500476c98
Bledi <bled.leka.al@gmail.com>
2020-06-28 10:08:27 -0500

    Version

--- samplecall.php
+++ samplecall.php
@@ -2,10 +2,8 @@
 /*
  * Sample call - Make sure to whitelist your servers ip address on your account in order to have proper responses
  */
-
-use \Emailcleaningclub\Verification\Apicall;
-
 require 'vendor/autoload.php';
+use Emailcleaningclub\Verification\Apicall;
 /* initialize the class with main configs */
 
 $Start = new Apicall("90C5626330E03D5C1799DF270AF7A114528B6F40");
